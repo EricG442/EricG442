d52383ae3a14637e3cb272c595ba5e27d0c2bf96
EricG442 <ericg206@outlook.com>
2026-07-14 08:41:26 -0700

    fix: update Navbar links to remove router basename

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ export default function Navbar() {
             <nav className="flex flex-col m-2">
                 <h1>eric.gish.dev</h1>
                 <ul className="flex space-x-4 m-auto">
-                    <li><a href="/EricG442/">Home</a></li>
-                    <li><a href="/EricG442/#contact">Contact</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/#contact">Contact</a></li>
                     <li><a href="https://github.com/ericg442" target="_blank" rel="noopener noreferrer">Github</a></li>
                 </ul>
             </nav>
